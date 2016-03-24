Naios/TrinityCore

Core/Database: Converted the database library to a shared library. * There is still the possibility to static link against database.

src/server/database/Database/Implementation/CharacterDatabase.h
@@ -595,7 +595,7 @@ enum CharacterDatabaseStatements
     MAX_CHARACTERDATABASE_STATEMENTS
 };
 
-class CharacterDatabaseConnection : public MySQLConnection
+class TRINITY_DATABASE_API CharacterDatabaseConnection : public MySQLConnection
 {
 public:
     typedef CharacterDatabaseStatements Statements;

src/server/database/Database/Implementation/LoginDatabase.h
@@ -147,7 +147,7 @@ enum LoginDatabaseStatements
     MAX_LOGINDATABASE_STATEMENTS
 };
 
-class LoginDatabaseConnection : public MySQLConnection
+class TRINITY_DATABASE_API LoginDatabaseConnection : public MySQLConnection
 {
 public:
     typedef LoginDatabaseStatements Statements;

rozalba-ng/noble-core

(cherry picked from commit 6cfea2d8f7de3de8206a1f4cf23a6d653814afe1)

src/server/database/Database/Implementation/CharacterDatabase.h
@@ -537,7 +537,7 @@ enum CharacterDatabaseStatements
     MAX_CHARACTERDATABASE_STATEMENTS
 };
 
-class CharacterDatabaseConnection : public MySQLConnection
+class TC_DATABASE_API CharacterDatabaseConnection : public MySQLConnection
 {
 public:
     typedef CharacterDatabaseStatements Statements;

src/server/database/Database/Implementation/LoginDatabase.h
@@ -116,7 +116,7 @@ enum LoginDatabaseStatements
     MAX_LOGINDATABASE_STATEMENTS
 };
 
-class LoginDatabaseConnection : public MySQLConnection
+class TC_DATABASE_API LoginDatabaseConnection : public MySQLConnection
 {
 public:
     typedef LoginDatabaseStatements Statements;