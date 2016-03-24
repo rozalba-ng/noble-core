Naios/TrinityCore

Core/Shared: Converted the shared library to a shared library ,-). * There is still the possibility to static link against shared.

src/server/shared/DataStores/DBCFileLoader.h
@@ -23,7 +23,7 @@
 #include "Utilities/ByteConverter.h"
 #include <cassert>
 
-class DBCFileLoader
+class TRINITY_SHARED_API DBCFileLoader
 {
     public:
         DBCFileLoader();

src/server/shared/Packets/ByteBuffer.h
@@ -37,7 +37,7 @@
 class MessageBuffer;
 
 // Root of ByteBuffer exception hierarchy
-class ByteBufferException : public std::exception
+class TRINITY_SHARED_API ByteBufferException : public std::exception
 {
 public:
     ~ByteBufferException() throw() { }
@@ -51,7 +51,7 @@ private:
     std::string msg_;
 };
 
-class ByteBufferPositionException : public ByteBufferException
+class TRINITY_SHARED_API ByteBufferPositionException : public ByteBufferException
 {
 public:
     ByteBufferPositionException(bool add, size_t pos, size_t size, size_t valueSize);
@@ -59,7 +59,7 @@ public:
     ~ByteBufferPositionException() throw() { }
 };
 
-class ByteBufferSourceException : public ByteBufferException
+class TRINITY_SHARED_API ByteBufferSourceException : public ByteBufferException
 {
 public:
     ByteBufferSourceException(size_t pos, size_t size, size_t valueSize);
@@ -67,7 +67,7 @@ public:
     ~ByteBufferSourceException() throw() { }
 };
 
-class ByteBuffer
+class TRINITY_SHARED_API ByteBuffer
 {
     public:
         static size_t const DEFAULT_SIZE = 0x1000;

rozalba-ng/noble-core

(cherry picked from commit 25b0e743b1a823b636ffc1ba17062f8d5ebe35ab)

src/server/shared/DataStores/DBCFileLoader.h
@@ -37,7 +37,7 @@ enum DbcFieldFormat
     FT_SQL_ABSENT='a'                                       //Used in sql format to mark column absent in sql dbc
 };
 
-class DBCFileLoader
+class TC_SHARED_API DBCFileLoader
 {
     public:
         DBCFileLoader();

src/server/shared/Packets/ByteBuffer.h
@@ -37,7 +37,7 @@
 class MessageBuffer;
 
 // Root of ByteBuffer exception hierarchy
-class ByteBufferException : public std::exception
+class TC_SHARED_API ByteBufferException : public std::exception
 {
 public:
     ~ByteBufferException() throw() { }
@@ -51,7 +51,7 @@ private:
     std::string msg_;
 };
 
-class ByteBufferPositionException : public ByteBufferException
+class TC_SHARED_API ByteBufferPositionException : public ByteBufferException
 {
 public:
     ByteBufferPositionException(bool add, size_t pos, size_t size, size_t valueSize);
@@ -59,7 +59,7 @@ public:
     ~ByteBufferPositionException() throw() { }
 };
 
-class ByteBufferSourceException : public ByteBufferException
+class TC_SHARED_API ByteBufferSourceException : public ByteBufferException
 {
 public:
     ByteBufferSourceException(size_t pos, size_t size, size_t valueSize);
@@ -67,7 +67,7 @@ public:
     ~ByteBufferSourceException() throw() { }
 };
 
-class ByteBuffer
+class TC_SHARED_API ByteBuffer
 {
     public:
         const static size_t DEFAULT_SIZE = 0x1000;