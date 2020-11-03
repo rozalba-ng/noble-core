34627160f3d48b900f8ad079fa6e07e5836abfa4
Саша <rozalba89@gmail.com>
2020-11-03 14:42:35 +0300

    gob move skip deleting

--- src/server/scripts/Commands/cs_gobject.cpp
+++ src/server/scripts/Commands/cs_gobject.cpp
@@ -520,18 +520,20 @@ public:
         object->Relocate(x, y, z, object->GetOrientation());
         object->SaveToDB();
 
+//        float customScale = object->GetCustomScale();
+
         // Generate a completely new spawn with new guid
         // 3.3.5a client caches recently deleted objects and brings them back to life
         // when CreateObject block for this guid is received again
         // however it entirely skips parsing that block and only uses already known location
-        object->Delete();
-
-        object = new GameObject();
-        if (!object->LoadGameObjectFromDB(guidLow, map))
-        {
-            delete object;
-            return false;
-        }
+//        object->Delete();
+//
+//        object = new GameObject();
+//        if (!object->LoadGameObjectFromDB(guidLow, map))
+//        {
+//            delete object;
+//            return false;
+//        }
 
         handler->PSendSysMessage(LANG_COMMAND_MOVEOBJMESSAGE, object->GetSpawnId(), object->GetGOInfo()->name.c_str(), object->GetSpawnId());
         return true;
