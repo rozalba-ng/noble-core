1a41243ec34bb2488ec7d7e13bf1c06f1d3f8711
Саша <rozalba89@gmail.com>
2021-07-04 13:25:41 +0300

    correct minus mod handle and enums refactoring fix

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13046,29 +13046,29 @@ void Unit::SetLevel(uint8 lvl)
 
 void Unit::SetRoleStat(uint8 stat, uint32 value, bool apply, bool update) // ROLE STAT SYSTEM
 {
-    TC_LOG_DEBUG("sql.sql", "[ %u ] entered func ENCHANT_VALUE", value);
+    TC_LOG_DEBUG("entities.player.items", "[ %u ] entered func ENCHANT_VALUE", value);
 	//if (HasAura(88009) && apply)
 	//	return;
 
 	//float oldRating = role_stats[stat];
 	if (apply) {
-        TC_LOG_ERROR("sql.sql", "[ %u ]  apply = true ENCHANT_VALUE", value);
+        TC_LOG_ERROR("entities.player.items", "[ %u ]  apply = true ENCHANT_VALUE", value);
 
         role_stats[stat] += value;
         if (role_stats[stat] > MAX_ROLE_STAT_VAL) {
             role_stats[stat] = MAX_ROLE_STAT_VAL;
         }
 	} else {
-        TC_LOG_ERROR("sql.sql", "[ %u ] apply = false ENCHANT_VALUE", value);
+        TC_LOG_ERROR("entities.player.items", "[ %u ] apply = false ENCHANT_VALUE", value);
 
 	    if (role_stats[stat] - value < 0) {
-            TC_LOG_ERROR("sql.sql", "[ %u ] role_stats[stat] - value < 0 ROLE_STAT", role_stats[stat]);
+            TC_LOG_ERROR("entities.player.items", "[ %u ] role_stats[stat] - value < 0 ROLE_STAT", role_stats[stat]);
             role_stats[stat] = 0;
 	    } else {
-            TC_LOG_ERROR("sql.sql", "[ %u ] role_stats[stat] - value > 0 ROLE_STAT", role_stats[stat]);
+            TC_LOG_ERROR("entities.player.items", "[ %u ] role_stats[stat] - value > 0 ROLE_STAT", role_stats[stat]);
             role_stats[stat] -= value;
 	    }
-        TC_LOG_ERROR("sql.sql", "[ %u ] final ROLE_STAT", role_stats[stat]);
+        TC_LOG_ERROR("entities.player.items", "[ %u ] final ROLE_STAT", role_stats[stat]);
 	}
 
 	if (Player* player = ToPlayer())
