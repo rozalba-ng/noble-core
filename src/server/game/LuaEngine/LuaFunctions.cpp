c3a07a5ccc87a8728814694103922a76236f02d0
Ezil Computer <izhevago@mail.ru>
2022-08-12 20:17:01 +0300

    fix

--- src/server/game/LuaEngine/LuaFunctions.cpp
+++ src/server/game/LuaEngine/LuaFunctions.cpp
@@ -1052,7 +1052,7 @@ ElunaRegister<Spell> SpellMethods[] =
     { "Cancel", &LuaSpell::Cancel },
     { "Cast", &LuaSpell::Cast },
     { "Finish", &LuaSpell::Finish },
-    { "DontFinishTurn", &LuaSpell:DontFinishTurn}
+    { "DontFinishTurn", &LuaSpell::DontFinishTurn },
     { NULL, NULL }
 };
 
