295d1f334e6e7802f8487f271ef12c1f7236489f
U I <dudeplayer65@gmail.com>
2023-01-18 20:16:32 +0000

    fixed upright word

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -87,7 +87,7 @@ enum Races
     RACE_ICE_TROLL          = 21,  // тирасец
     RACE_BLOODELFN         = 22,  // новый БЭ
     RACE_THINHUMAN          = 23,  // тонкий человек
-    RACE_UPGRIGHT          = 24,  // прямой орк
+    RACE_UPRIGHT          = 24,  // прямой орк
     RACE_HALFELF          = 25  // полуэльф
 };
 
@@ -103,8 +103,8 @@ enum Races
      (1<<(RACE_BROKEN-1))   |(1<<(RACE_SKELETON-1))           |(1<<(RACE_VRYKUL-1))    | \
      (1<<(RACE_TUSKARR-1))  |(1<<(RACE_FOREST_TROLL-1))       |(1<<(RACE_TAUNKA-1))    | \
      (1<<(RACE_NORTHREND_SKELETON-1)) | (1<<(RACE_ICE_TROLL-1))	| \
-	 (1<<(RACE_BLOODELFN-1))  |(1<<(RACE_THINHUMAN-1))       |(1<<(RACE_UPGRIGHT-1))    | \
-	 (1<<(RACE_HALFELF-1)))     | \
+	 (1<<(RACE_BLOODELFN-1))  |(1<<(RACE_THINHUMAN-1))       |(1<<(RACE_UPRIGHT-1))    | \
+	 (1<<(RACE_HALFELF-1)))    | \
 
 #define RACEMASK_ALLIANCE \
     ((1<<(RACE_HUMAN-1)) | (1<<(RACE_DWARF-1)) | (1<<(RACE_NIGHTELF-1)) | \
@@ -113,7 +113,7 @@ enum Races
      (1<<(RACE_BROKEN-1))| \
      (1<<(RACE_SKELETON-1))|(1<<(RACE_VRYKUL-1))|(1<<(RACE_TUSKARR-1))| \
      (1<<(RACE_FOREST_TROLL-1))| (1<<(RACE_NORTHREND_SKELETON-1))| (1<<(RACE_ICE_TROLL-1))| \
-	 (1<<(RACE_BLOODELFN-1)) | (1<<(RACE_THINHUMAN-1)) | (1<<(RACE_HALFELF-1)))     | \
+	 (1<<(RACE_BLOODELFN-1))| (1<<(RACE_THINHUMAN-1))| (1<<(RACE_HALFELF-1)))| \
 
 #define RACEMASK_HORDE RACEMASK_ALL_PLAYABLE & ~RACEMASK_ALLIANCE
 
